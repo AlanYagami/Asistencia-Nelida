9281edd021d030b131fbed2b5d78d0aa926548d1
AlanYamami <20223TN046@utez.edu.mx>
2025-05-22 18:23:40 -0600

    feat/back-create

---
+++ BackPaseLista/src/main/java/mx/edu/utez/backpaselista/BackPaseListaApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.backpaselista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPaseListaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPaseListaApplication.class, args);
+    }
+
+}
---
+++ BackPaseLista/src/test/java/mx/edu/utez/backpaselista/BackPaseListaApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.backpaselista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPaseListaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
